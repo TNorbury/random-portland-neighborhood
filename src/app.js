94ec0bb24aef1feddae3c4944471c31ecaddbb92
Tyler Norbury <tylernorbury96@gmail.com>
2020-10-03 20:46:50 -0700

    Use handlebars to make a simple web page

--- src/app.js
+++ src/app.js
@@ -1,10 +1,25 @@
-const express = require("express");
 const pickNeighborhood = require("./pick-neighborhood");
+
+const path = require("path");
+
+const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
 
+const viewsPath = path.join(__dirname, "./views");
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+
+
 app.get("", (req, res) => {
-    res.send(pickNeighborhood());
+    const {section, neighborhood, mapsUrl} = pickNeighborhood()
+    // res.send();
+    res.render("index", {
+        section,
+        neighborhood,
+        mapsUrl
+    })
 });
 
 app.listen("3000", () => {
